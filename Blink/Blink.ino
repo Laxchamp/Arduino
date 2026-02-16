2b327b637e1c224732e551154c1be87a3e9c73db
Rowe <coledrowe@gmail.com>
2026-02-16 19:24:40 +0000

    Blink Stable

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,6 +1,7 @@
 /*
   Blink
-
+By: Cole Rowe
+Date: 2.16.26
   Turns an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
